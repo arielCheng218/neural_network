8a1ee88e24147a62600bc737fcb5336c53580800
Ariel <Cheng>
2021-10-19 19:43:54 +0800

    Layer object

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 
 import numpy as np
-from Neuron import *
+from Layer import *
 
 def main():
-    inputs = [1, 2, 3]
-    weights = [0.2, 0.8, -0.5]
-    n = Neuron(weights, bias=2)
-    print(n.get_value(inputs))
 
-if __name__ == "__main__":
-    main()
+  X = [
+    [2, 3, 2, 2.5],
+    [2.0, 3.4, 1, 3],
+    [1, 3, 2, 6]
+  ]
+
+  layer1 = Layer(4, 5)
+  layer2 = Layer(5, 2)
+
+  layer1.forward(X)
+  layer2.forward(layer1.output)
+
+  print(layer1.output)
+
+main()
